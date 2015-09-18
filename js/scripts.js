835a87d89b0976f75a4ac386420e2caa27ef2a62
Leah Klamkin <leahcodes@gmail.com>
2015-09-18 10:28:35 -0700

    Add receipt on HTML page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-function Pizza(size) {
+function Pizza(size, cost) {
   this.size = size;
+  this.cost = 10;
+  // this.toppings = [];
 }
 
 function Topping(topping) {
@@ -11,7 +13,28 @@ function Order(customerName, numberOfPizzas) {
   this.numberOfPizzas = numberOfPizzas;
   this.price = null;
 }
-//
-// function resetFields() {
-//   $("input#   ").val("");
-// }
+
+function resetFields() {
+  $("select#size-dropdown").val("");
+  $("input#topping1").val("");
+  $("input#topping2").val("");
+  $("input#topping3").val("");
+  $("input#topping4").val("");
+  $("input#topping5").val("");
+  $("input#topping6").val("");
+  $("input#topping7").val("");
+  $("input#topping8").val("");
+  $("input#topping9").val("");
+  $("input#topping10").val("");
+  $("input#topping11").val("");
+  $("input#numberOfPizzas").val("");
+  $("input#customerName").val("");
+}
+
+$(document).ready(function() {
+  $("form#build-order").submit(function() {
+    event.preventDefault();
+
+    var size = $("input#size-dropdown option:selected").val();
+  });
+});
